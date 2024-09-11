5d9f90df7957a03aea3d4f421c783bc5786c42ec
Tejan195 <tejan.thapa.555@gmail.com>
2024-09-11 10:28:28 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App.jsx'
 import 'aos/dist/aos.css'; 
 import AOS from 'aos'; 
